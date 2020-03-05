9397ac6e8ed7ae7d57cfaa562b15177d55147133
Laurent Carré <30366677+sterwen@users.noreply.github.com>
2020-03-05 16:04:20 +0100

    Update version and copyright

--- python_transport/wirepas_gateway/__about__.py
+++ python_transport/wirepas_gateway/__about__.py
@@ -2,9 +2,12 @@
     .. Copyright:
         Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
         See file LICENSE for full license details.
+        Version with the addition and correction for the Maersk Beacon application
+        Addition Corrections
+        Copyright 2020 Solid Run Ltd, Sterwen Technology SAS
 """
 
-__author__ = "Wirepas Ltd"
+__author__ = "Wirepas Ltd, Solid Run Ltd, Sterwen Technology SAS"
 __author_email__ = "opensource@wirepas.com"
 __classifiers__ = [
     "Development Status :: 5 - Production/Stable",
@@ -13,11 +16,11 @@ __classifiers__ = [
     "Topic :: Software Development :: Libraries",
     "Programming Language :: Python :: 3",
 ]
-__copyright__ = "2019 Wirepas Ltd"
-__description__ = "Wirepas gateway transport service that connects the local dbus to a remote MQTT broker."
+__copyright__ = "2019 Wirepas Ltd, 2020 Sterwen Technology SAS, Solid Run Ltd"
+__description__ = "Wirepas gateway transport service that connects the local dbus to a remote MQTT broker. Maersk Beacon edition"
 __license__ = "Apache-2"
 __pkg_name__ = "wirepas_gateway"
-__title__ = "Wirepas Gateway Transport Service"
+__title__ = "Wirepas Gateway Transport Service for Maersk Beacon"
 __url__ = "https://github.com/wirepas/gateway"
-__version__ = "1.2.0"
+__version__ = "M-V1.15"
 __keywords__ = "wirepas connectivity iot mesh"
